d1e413928539069ca3f8db556ebf4d3f61da3f0a
Jânio <janioofi@gmail.com>
2024-05-03 20:55:30 -0300

    Criado projeto e componentes entrada e saida

---
+++ src/app/components/entrada/entrada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entrada',
+  standalone: true,
+  imports: [],
+  templateUrl: './entrada.component.html',
+  styleUrl: './entrada.component.css'
+})
+export class EntradaComponent {
+
+}
---
+++ src/app/components/saida/saida.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saida',
+  standalone: true,
+  imports: [],
+  templateUrl: './saida.component.html',
+  styleUrl: './saida.component.css'
+})
+export class SaidaComponent {
+
+}
